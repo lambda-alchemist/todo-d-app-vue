c24b69916f75b902271dd548a97c8845ae9f718d
Guilherme M. Miranda <alchemist.software@proton.me>
2023-04-05 15:45:04 -0300

    changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts"></script>
 <template>
 	<nav class="flex w-full bg-emerald-950 font-sans font-semibold text-black">
-		<ul class="flex flex-row">
-			<li
+		<div class="flex flex-row">
+			<a
+				href=""
 				class="m-0.5 my-1.5 rounded bg-cyan-950 p-2 transition-colors duration-100 hover:bg-emerald-700"
+				>Docs</a
 			>
-				<a href="">Docs</a>
-			</li>
-			<li
+			<a
+				href=""
 				class="m-0.5 my-1.5 rounded bg-cyan-950 p-2 transition-colors duration-100 hover:bg-emerald-700"
+				>Contribute</a
 			>
-				<a href="">Contribute</a>
-			</li>
-			<li
+			<a
+				href=""
 				class="m-0.5 my-1.5 rounded bg-cyan-950 p-2 transition-colors duration-100 hover:bg-emerald-700"
+				>About Us</a
 			>
-				<a href="">About Us</a>
-			</li>
-		</ul>
+		</div>
 	</nav>
 </template>
