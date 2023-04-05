94d265727aa18a6ca1b04cbc3c7b159c060dc00b
Guilherme M. Miranda <alchemist.software@proton.me>
2023-04-05 15:33:16 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import HelloUser from './components/HelloUser.vue'
-import CountButton from './components/CountButton.vue'
-import GenericCard from './components/GenericCard.vue'
+import HelloUser from './components/GreeterUser.vue'
+import CountButton from './components/ButtonCount.vue'
+import GenericCard from './components/CardGeneric.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-	<div id="root" class="flex flex-col items-center p-4">
-		<GenericCard>
-			<CountButton/>
-		</GenericCard>
-		<HelloUser username="Guilherme" />
+	<div id="root" class="flex flex-col items-center">
+		<NavBar/>
+		<div class="p-4">
+			<GenericCard>
+				<CountButton/>
+			</GenericCard>
+			<HelloUser username="Guilherme" />
+		</div>
 	</div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+<template>
+	<nav class="flex w-full bg-emerald-950 font-sans font-semibold text-black">
+		<ul class="flex flex-row">
+			<li
+				class="m-0.5 my-1.5 rounded bg-cyan-950 p-2 transition-colors duration-100 hover:bg-emerald-700"
+			>
+				<a href="">Docs</a>
+			</li>
+			<li
+				class="m-0.5 my-1.5 rounded bg-cyan-950 p-2 transition-colors duration-100 hover:bg-emerald-700"
+			>
+				<a href="">Contribute</a>
+			</li>
+			<li
+				class="m-0.5 my-1.5 rounded bg-cyan-950 p-2 transition-colors duration-100 hover:bg-emerald-700"
+			>
+				<a href="">About Us</a>
+			</li>
+		</ul>
+	</nav>
+</template>
