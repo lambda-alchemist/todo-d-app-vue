e42dedbb5fc2917c0eea884d956586a8f933af11
Guilherme M. Miranda <alchemist.software@proton.me>
2023-04-17 18:02:05 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloUser from './components/GreeterUser.vue'
-import CountButton from './components/ButtonCount.vue'
-import GenericCard from './components/CardGeneric.vue'
+import GreeterUser from './components/GreeterUser.vue'
+import ButtonCount from './components/ButtonCount.vue'
+import CardGeneric from './components/CardGeneric.vue'
 import NavBar from './components/NavBar.vue';
 </script>
 
@@ -9,10 +9,10 @@ import NavBar from './components/NavBar.vue';
 	<div id="root" class="flex flex-col items-center">
 		<NavBar/>
 		<div class="p-4">
-			<GenericCard>
-				<CountButton/>
-			</GenericCard>
-			<HelloUser username="Guilherme" />
+			<CardGeneric>
+				<ButtonCount/>
+			</CardGeneric>
+			<GreeterUser username="Alchemist" />
 		</div>
 	</div>
 </template>
