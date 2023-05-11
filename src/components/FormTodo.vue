f3833d1d26ae2b42d7f47940dfabe954c7fff6ce
Guilherme M. Miranda <alchemist.software@proton.me>
2023-05-11 19:04:31 -0300

    minor tweaks

--- src/components/FormTodo.vue
+++ src/components/FormTodo.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts"></script>
 <template>
-	<form
-		action="/"
-		class="m-2 flex flex-col p-2 text-center"
-	>
+	<form class="m-2 flex flex-col p-2 text-center">
+		<label for="task-name">Create todo task:</label>
 		<input
+			placeholder="Insert name here"
+			name="task-name"
 			type="text"
 			class="my-2 rounded bg-slate-700 p-2"
 		/>
+		<label for="task-date"></label>
 		<input
-			type="text"
+			name="task-date"
+			type="date"
 			class="my-2 rounded bg-slate-700 p-2"
 		/>
 		<button
+			@click.prevent="$emit('submit')"
 			type="button"
 			class="m-1 self-center rounded-sm bg-emerald-700 p-2 font-semibold text-black hover:bg-teal-800"
 		>
