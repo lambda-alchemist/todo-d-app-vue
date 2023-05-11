69d976d98be15da6bfcda369eea7f37effce3c68
Guilherme M. Miranda <alchemist.software@proton.me>
2023-05-11 16:58:19 -0300

    changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
-
+/** @type {import('@types/eslint').ESLint.ConfigData} */
 module.exports = {
 	root: true,
-	'extends': [
+	extends: [
 		'plugin:vue/vue3-essential',
 		'eslint:recommended',
 		'@vue/eslint-config-typescript',
